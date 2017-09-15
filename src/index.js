93ab94f65e8ed08ec6da7915f15b70ce095cb978
Akhil <akhil.sudh@gmail.com>
2017-09-15 18:07:43 +0530

    Youtube API called

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
+//All the data handling shit must be done in the highest most component
 const API_KEY = 'AIzaSyBMsjET-0KMRF9f3DGl92muiHytGbZ3TC8';
+
+//Youtube API called here
+YTSearch({ key: API_KEY, term: 'surfboards'}, function(data){
+	console.log(data);
+});
 
 //Create a new component and this 
 //component must produce some html
