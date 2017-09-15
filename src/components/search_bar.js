b0c527786bfb8f7f1d59045b3b0abd8089dfc2bf
Akhil <akhil.sudh@gmail.com>
2017-09-15 15:28:46 +0530

    Added ES6 class based support

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
+class SearchBar extends React.Component {
+	render() {
+		return <input />
+	}
+}
+
+/*
 const SearchBar = () => {
 	return <input />; 
 };
-
+*/
 export default SearchBar;
